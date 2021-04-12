e03a9d376b9e688b7427d4795073bbcbc30736fe
Lonyphus <lonyphus@gmail.com>
2021-04-13 00:46:47 +0800

    fix the annoying announcement test1

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import discord
-from discord.ext import commands
 import os
 
 token = os.getenv("DC_TOKEN")
 client = discord.Client()
 
+######################################################
 # channel = client.get_channel(828677514184491059)
-##################
 # channel 不能宣告出來使用 會噴錯 我也不知道為什麼
-##################
+######################################################
 
 
-@client.event
-async def on_ready():
-    print('>> {0.user} 已經登入 <<'.format(client))
-    await client.get_channel(828677514184491059).send(">> 霸氣測試 beta 0.2 <<")
+# @client.event
+# async def on_ready():
+#     print('>> {0.user} 已經登入 <<'.format(client))
+    # await client.get_channel(828677514184491059).send(">> 霸氣測試 beta 0.2 <<")
 
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if after.channel is not None:
-        await client.get_channel(828677514184491059).send(f"{member} 進來了")
-    if after.channel is None:
-        await client.get_channel(828677514184491059).send(f"{member} 出來了")
+    print(member)
+    print(type(member))
+    # if after.channel is not None:
+    #     await client.get_channel(828677514184491059).send(f"{member} 進來了")
+    # if after.channel is None:
+    #     await client.get_channel(828677514184491059).send(f"{member} 出來了")
 
 
 client.run(token)
